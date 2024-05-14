74ec7ccd8e8e49d0772a306efd7d53e9dcac1e32
Tilman Dreßler <t.dressler@kithara.com>
2024-05-14 09:58:43 +0200

    main.cpp: Introduce Generate Wall

--- main.cpp
+++ main.cpp
@@ -28,3 +28,26 @@ struct Object : Drawable {
 
   TLambda onClick_;
 };
+
+template <typename TLambda>
+struct Wall : Object<TLambda> {
+  Wall(Vector2f pos, bool horizontal, TLambda lambda) : Object<TLambda>(lambda) {
+    if (horizontal)
+      wall = RectangleShape(Vector2f{ 10, 100 });
+    else
+      wall = RectangleShape(Vector2f{ 100, 10 });
+    wall.setFillColor(Color::Black);
+    wall.setOrigin(wall.getLocalBounds().width / 2.0f, wall.getLocalBounds().height / 2.0f);
+    wall.setPosition(pos);
+  }
+
+  void draw(RenderTarget& target, RenderStates states) const {
+    target.draw(wall, states);
+  }
+
+  void move(Vector2f pos) {
+    wall.setPosition(pos);
+  }
+private:
+  RectangleShape wall;
+};
