00d6427be25c57ed82d23cdb75e2849de9e78215
Tilman Dreßler <t.dressler@kithara.com>
2024-05-14 09:58:05 +0200

    main.cpp: Introduce Object: Drawable

--- main.cpp
+++ main.cpp
@@ -4,3 +4,27 @@
 #include <tuple>
 
 using namespace sf;
+template <typename TLambda>
+struct Object : Drawable {
+  Object(TLambda onClick) : onClick_(onClick) {}
+
+  static bool isMouseOver(RenderWindow* pWindow, const Shape& shape) {
+    Vector2f mousePos = pWindow->mapPixelToCoords(Mouse::getPosition(*pWindow));
+    FloatRect buttonRect = shape.getGlobalBounds();
+    return buttonRect.contains(mousePos);
+  }
+
+  template <typename... T>
+  std::optional<Drawable> isClicked(RenderWindow* pWindow, const Shape& shape, T... args) {
+    if (isMouseOver(pWindow, shape)) {
+      if (Mouse::isButtonPressed(Mouse::Left)) {
+        return onClick_(args...);
+      }
+    }
+    else {
+      return std::nullopt;
+    }
+  }
+
+  TLambda onClick_;
+};
