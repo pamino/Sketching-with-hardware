7a6e5d9f6233dba6ca6f2c216cbebe2fa528ce95
Tilman Dreßler <t.dressler@kithara.com>
2024-06-05 10:36:12 +0200

    Simulator.cpp: refactor lambdas in std::function

--- Simulator/Simulator.cpp
+++ Simulator/Simulator.cpp
@@ -1,5 +1,13 @@
 #include "Simulator.h"
 
+using namespace sf;
+
+static sf::Simulator sim{};
+
+static const auto moveObj = [&](sf::Object* pClickable) -> sf::Object* {
+  pClickable->move2(sim.window.mapPixelToCoords(sf::Mouse::getPosition(sim.window)));
+  return nullptr; };
+
 //--------------------------------------------------------------------------------------------------------------
 // Object
 //--------------------------------------------------------------------------------------------------------------
@@ -47,11 +55,71 @@ void Object::keyBoardMove(std::vector<std::shared_ptr<Drawable>>* pDrawables) {
 }
 
 //--------------------------------------------------------------------------------------------------------------
+// Clickable
+//--------------------------------------------------------------------------------------------------------------
+
+//------ invoke ------
+Object* Clickable::invoke() {
+  static Clickable* mutex = this;
+  static auto timeSinceLastInvoke = 0ms;
+  static auto lastTime = std::chrono::high_resolution_clock::now();
+
+  timeSinceLastInvoke = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - lastTime);
+  if (mutex != this && timeSinceLastInvoke < 100ms)
+    return nullptr;
+  mutex = this;
+
+  timeSinceLastInvoke = 0ms;
+  lastTime = std::chrono::high_resolution_clock::now();
+
+  if (!delay())
+    return onClick_(this);
+
+  return nullptr;
+}
+
+//--------------------------------------------------------------------------------------------------------------
+// GenerateDrawable
+//--------------------------------------------------------------------------------------------------------------
+
+//------ GenerateDrawable ------
+GenerateDrawable::GenerateDrawable(Vector2f pos, Vector2f size, std::function<Object*(Object*)> lambda, const std::string& text)
+  : Clickable(lambda) {
+  Clickable::pShape_ = std::make_shared<CircleShape>(50);
+  Clickable::pShape_->setPosition(pos);
+  Clickable::pShape_->setFillColor(Color::Red);
+
+  text_.setFont(font_);
+  text_.setCharacterSize(15); // Adjust the size if necessary
+  text_.setFillColor(Color::Black);
+  text_.setString(text);
+
+  // Center the text in the circle
+  FloatRect textRect = text_.getLocalBounds();
+  text_.setOrigin(textRect.left + textRect.width / 2.0f, textRect.top + textRect.height / 2.0f);
+  text_.setPosition(pos + Vector2f(10, 40));
+}
+
+//------ delay ------
+bool GenerateDrawable::delay() const {
+  static auto start = std::chrono::high_resolution_clock::now();
+  static auto end = std::chrono::high_resolution_clock::now() + 500ms;
+
+  end = std::chrono::high_resolution_clock::now();
+  if (std::chrono::duration_cast<std::chrono::milliseconds>(end - start) > 500ms) {
+    start = std::chrono::high_resolution_clock::now();
+    return false;
+  }
+  return true;
+}
+
+//--------------------------------------------------------------------------------------------------------------
 // DistanceSensor
 //--------------------------------------------------------------------------------------------------------------
 
 //------ DistanceSensor ------
-DistanceSensor::DistanceSensor(Vector2f direct, Vector2f pos, int radius) {
+DistanceSensor::DistanceSensor(Vector2f direct, Vector2f pos, int radius, std::vector<std::shared_ptr<Drawable>>* pWalls)
+  : pWalls_(pWalls) {
   Object::pShape_ = std::make_shared<CircleShape>(radius);
   Object::pShape_->setFillColor(Color::Red);
   Object::pShape_->setPosition(pos);
@@ -63,22 +131,23 @@ DistanceSensor::DistanceSensor(Vector2f direct, Vector2f pos, int radius) {
 DistanceSensor& DistanceSensor::operator = (const DistanceSensor& rhs) {
   this->pShape_ = rhs.pShape_;
   this->front_ = rhs.front_;
+  this->pWalls_ = rhs.pWalls_;
   return *this;
 }
 
 //------ measureDistance ------
-std::optional<float> DistanceSensor::measureDistance(std::vector<std::shared_ptr<Drawable>>* pWalls) {
-  for (auto pWall : *pWalls) {
+float DistanceSensor::measureDistance() const {
+  for (auto pWall : *pWalls_) {
     if (auto distance = rectangleDistance(*(RectangleShape*)((Object*)pWall.get())->shape().get());
       distance.has_value())
       return distance.value();
   }
 
-  return std::nullopt;
+  return 10000;
 }
 
 //------ rectangleDistance ------
-std::optional<float> DistanceSensor::rectangleDistance(const RectangleShape& rectangle) {
+std::optional<float> DistanceSensor::rectangleDistance(const RectangleShape& rectangle) const {
   float width = rectangle.getSize().x;
   float height = rectangle.getSize().y;
 
@@ -123,11 +192,131 @@ std::optional<float> DistanceSensor::rectangleDistance(const RectangleShape& rec
   return std::nullopt; // No intersection found
 }
 
-static Simulator sim{};
+//--------------------------------------------------------------------------------------------------------------
+// Car
+//--------------------------------------------------------------------------------------------------------------
 
-static const auto moveObj = [&](Object* pClickable) -> Object* {
-  pClickable->move2(sim.window.mapPixelToCoords(Mouse::getPosition(sim.window)));
-  return nullptr; };
+//------ Car ------
+Car::Car(Vector2f pos, std::function<Object*(Object*)> lambda, std::vector<std::shared_ptr<Drawable>>* pWalls)
+  : Clickable(lambda), sensorsText_(5) {
+  Clickable::pShape_ = std::make_shared<RectangleShape>(Vector2f{75, 100});
+
+  Clickable::pShape_->setFillColor(Color::Blue);
+  Clickable::pShape_->setOrigin(Clickable::pShape_->getLocalBounds().width / 2.0f,
+    Clickable::pShape_->getLocalBounds().height / 2.0f);
+  Clickable::pShape_->setPosition(pos);
+
+  auto bounds = Clickable::pShape_->getGlobalBounds();
+  int radius = 10;
+  sensors_.push_back(DistanceSensor(Vector2f(1, 0), Vector2f(0, 0), radiusSensors, pWalls));
+  sensors_.push_back(DistanceSensor(Vector2f(-1, 0), Vector2f(0, 0), radiusSensors, pWalls));
+  sensors_.push_back(DistanceSensor(Vector2f(1, 0), Vector2f(0, 0), radiusSensors, pWalls));
+  sensors_.push_back(DistanceSensor(Vector2f(-1, 0), Vector2f(0, 0), radiusSensors, pWalls));
+  sensors_.push_back(DistanceSensor(Vector2f(0, -1), Vector2f(0, 0), radiusSensors, pWalls));
+
+  int yPos = 300;
+  for (auto& text : sensorsText_) {
+    text.setFont(font_);
+    text.setCharacterSize(24);
+    text.setFillColor(Color::Black);
+    text.setPosition(Vector2f(1200, yPos));
+    yPos -= 50;
+  }
+
+  updateSensorPositions();
+}
+
+//------ update ------
+void Car::update(std::vector<std::shared_ptr<Drawable>>* pWalls) {
+  int i = 0;
+  std::vector<std::string> names{"Top: ", "Left Bottom: ", "Right Bottom: ", "Left Top: ", "Right Top: "};
+  for (auto&& [sensor, name] : std::views::zip(sensors_, std::views::reverse(names))) {
+    sensorsText_[i].setString(name + std::to_string(sensor.measureDistance()));
+    ++i;
+  }
+}
+
+//------ draw ------
+void Car::draw(RenderTarget& target, RenderStates states) const {
+  Object::draw(target, states);
+  for (auto& sensor : sensors_)
+    sensor.draw(target, states);
+
+  for (auto& text : sensorsText_)
+    target.draw(text);
+}
+
+//------ collides ------
+bool Car::collides(FloatRect rect) {
+  for (auto& edge : edges()) {
+    if (rect.contains(edge))
+      return true;
+  }
+  return false;
+}
+
+//------ edges ------
+std::vector<Vector2f> Car::edges() {
+  RectangleShape car = *((RectangleShape*)Object::pShape_.get());
+  return {
+    car.getTransform().transformPoint(Vector2f(0, 0)), // top left
+    car.getTransform().transformPoint(Vector2f(car.getSize().x, 0)), // top right
+    car.getTransform().transformPoint(Vector2f(0, car.getSize().y)), // bottom left
+    car.getTransform().transformPoint(Vector2f(car.getSize().x, car.getSize().y)), // bottom right
+    car.getTransform().transformPoint(sf::Vector2f(car.getSize().x / 2, 0)) // top middle
+  };
+}
+
+//------ updateSensorPositions ------
+void Car::updateSensorPositions() {
+  auto bounds = Clickable::pShape_->getGlobalBounds();
+  sensors_[0].move2(Vector2f(edges()[0].x - radiusSensors, edges()[0].y - radiusSensors));
+  sensors_[1].move2(Vector2f(edges()[1].x - radiusSensors, edges()[1].y - radiusSensors));
+  sensors_[2].move2(Vector2f(edges()[2].x - radiusSensors, edges()[2].y - radiusSensors));
+  sensors_[3].move2(Vector2f(edges()[3].x - radiusSensors, edges()[3].y - radiusSensors));
+  sensors_[4].move2(Vector2f(edges()[4].x - radiusSensors, edges()[4].y - radiusSensors));
+}
+
+//------ turn ------
+void Car::turn(bool right) {
+  Object::turn(right);
+  if (right)
+    ((RectangleShape*)(Object::pShape_.get()))->rotate(Object::turnSpeed_);
+  else
+    ((RectangleShape*)(Object::pShape_.get()))->rotate(-Object::turnSpeed_);
+
+  updateSensorPositions();
+  for (auto& sensor : sensors_)
+    sensor.turn(right);
+}
+
+//--------------------------------------------------------------------------------------------------------------
+// Simulator
+//--------------------------------------------------------------------------------------------------------------
+
+//------ Simulator ------
+Simulator::Simulator() : window(sf::VideoMode(1600, 1200), "Labyrinth") {
+  auto moveObj = [&](Object* pClickable) -> Object* {
+    pClickable->move2(window.mapPixelToCoords(Mouse::getPosition(window)));
+    return nullptr; };
+
+  auto createHWall = [moveObj](Object*) { return new Wall(Vector2f{400, 300}, false, moveObj); };
+  auto createVWall = [moveObj](Object*) { return new Wall(Vector2f{400, 300}, true, moveObj); };
+
+  std::shared_ptr<Drawable> horizontalGenerator(new GenerateDrawable(Vector2f{50, 50}, Vector2f{50, 50}, createHWall,
+    "   generate \nhorizontal wall"));
+  std::shared_ptr<Drawable> verticalGenerator(new GenerateDrawable(Vector2f{50, 150}, Vector2f{50, 50}, createVWall,
+    "   generate \nvertical wall"));
+  car = std::shared_ptr<Drawable>(new Car(Vector2f{500, 500}, moveObj, &walls));
+
+  clickables.push_back(std::move(horizontalGenerator));
+  clickables.push_back(std::move(verticalGenerator));
+  clickables.push_back(car);
+}
+
+//--------------------------------------------------------------------------------------------------------------
+// main
+//--------------------------------------------------------------------------------------------------------------
 
 void eventLoop() {
   while (sim.window.isOpen()) {
@@ -139,7 +328,7 @@ void eventLoop() {
 
     sim.window.clear(Color::White);
 
-    ((Car<decltype(moveObj)>*)(sim.car.get()))->update(&sim.walls);
+    ((Car*)(sim.car.get()))->update(&sim.walls);
 
     for (auto& pDrawable : sim.clickables)
       sim.window.draw(*pDrawable);
--- Simulator/Simulator.h
+++ Simulator/Simulator.h
@@ -6,26 +6,28 @@
 #include <tuple>
 #include <chrono>
 #include <numeric>
+#include <functional>
 
-using namespace sf;
 using namespace std::chrono_literals;
+
+namespace sf {
 
 static Font font_;
 
 //------ rotateVector ------
-Vector2f rotateVector(const Vector2f& vector, float angleDegrees) {
+inline Vector2f rotateVector(const Vector2f& vector, float angleDegrees) {
   Transform rotation;
   rotation.rotate(angleDegrees);
   return rotation.transformPoint(vector);
 }
 
 //------ normalizeVector ------
-sf::Vector2f normalizeVector(const sf::Vector2f& vector) {
+inline Vector2f normalizeVector(const Vector2f& vector) {
   float magnitude = std::sqrt(vector.x * vector.x + vector.y * vector.y);
   if (magnitude == 0) {
-    return sf::Vector2f(0.f, 0.f);
+    return Vector2f(0.f, 0.f);
   }
-  return sf::Vector2f(vector.x / magnitude, vector.y / magnitude);
+  return Vector2f(vector.x / magnitude, vector.y / magnitude);
 }
 
 //--------------------------------------------------------------------------------------------------------------
@@ -37,12 +39,12 @@ public:
   bool isMouseOver(RenderWindow* pWindow);
   Object* isClicked(RenderWindow* pWindow);
 
-  virtual Object* invoke()                              { return nullptr; }
+  virtual Object* invoke() { return nullptr; }
 
-  std::shared_ptr<Shape> shape()                        { return pShape_; }
+  std::shared_ptr<Shape> shape() { return pShape_; }
 
-  virtual void move2(Vector2f pos)                      { pShape_->setPosition(pos); }
-  virtual void move(Vector2f pos)                       { pShape_->setPosition(pShape_->getPosition() + pos); }
+  virtual void move2(Vector2f pos) { pShape_->setPosition(pos); }
+  virtual void move(Vector2f direct) { pShape_->setPosition(pShape_->getPosition() + direct); }
   virtual void turn(bool right) {
     front_ = right ? rotateVector(front_, turnSpeed_) : rotateVector(front_, -turnSpeed_);
     front_ = normalizeVector(front_);
@@ -52,10 +54,10 @@ public:
 
   void draw(RenderTarget& target, RenderStates states) const override { target.draw(*pShape_, states); }
 
-  virtual bool collides(FloatRect rect)                 { return pShape_->getGlobalBounds().intersects(rect); }
+  virtual bool collides(FloatRect rect) { return pShape_->getGlobalBounds().intersects(rect); }
 
-  float x() const                                       { return pShape_->getPosition().x; }
-  float y() const                                       { return pShape_->getPosition().y; }
+  float x() const { return pShape_->getPosition().x; }
+  float y() const { return pShape_->getPosition().y; }
 
 protected:
   std::shared_ptr<Shape> pShape_{nullptr};
@@ -68,54 +70,31 @@ protected:
 // Clickable
 //--------------------------------------------------------------------------------------------------------------
 
-template <typename TLambda>
 struct Clickable : Object {
-  Clickable(TLambda onClick) : onClick_(onClick) {}
+  Clickable(std::function<Object*(Object*)> onClick) : onClick_(onClick) {}
 
   Object* invoke() override;
-  virtual bool delay() const                            { return false; }
+  virtual bool delay() const { return false; }
 
 private:
-  TLambda onClick_;
+  std::function<Object*(Object*)> onClick_;
 };
-
-//------ invoke ------
-template <typename TLambda>
-Object* Clickable<TLambda>::invoke() {
-  static Clickable* mutex = this;
-  static auto timeSinceLastInvoke = 0ms;
-  static auto lastTime = std::chrono::high_resolution_clock::now();
-
-  timeSinceLastInvoke = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - lastTime);
-  if (mutex != this && timeSinceLastInvoke < 100ms)
-    return nullptr;
-  mutex = this;
-
-  timeSinceLastInvoke = 0ms;
-  lastTime = std::chrono::high_resolution_clock::now();
-
-  if (!delay())
-    return onClick_(this);
-
-  return nullptr;
-}
 
 //--------------------------------------------------------------------------------------------------------------
 // Wall
 //--------------------------------------------------------------------------------------------------------------
 
-template <typename TLambda>
-struct Wall : public Clickable<TLambda> {
-  Wall(Vector2f pos, bool horizontal, TLambda lambda) : Clickable<TLambda>(lambda) {
+struct Wall : public Clickable {
+  Wall(Vector2f pos, bool horizontal, std::function<Object*(Object*)> lambda) : Clickable(lambda) {
     if (horizontal)
-      Clickable<TLambda>::pShape_ = std::make_shared<RectangleShape>(Vector2f{ 20, 200 });
+      Clickable::pShape_ = std::make_shared<RectangleShape>(Vector2f{20, 200});
     else
-      Clickable<TLambda>::pShape_ = std::make_shared<RectangleShape>(Vector2f{ 200, 20 });
+      Clickable::pShape_ = std::make_shared<RectangleShape>(Vector2f{200, 20});
 
-    Clickable<TLambda>::pShape_->setFillColor(Color::Black);
-    Clickable<TLambda>::pShape_->setOrigin(Clickable<TLambda>::pShape_->getLocalBounds().width / 2.0f,
-      Clickable<TLambda>::pShape_->getLocalBounds().height / 2.0f);
-    Clickable<TLambda>::pShape_->setPosition(pos);
+    Clickable::pShape_->setFillColor(Color::Black);
+    Clickable::pShape_->setOrigin(Clickable::pShape_->getLocalBounds().width / 2.0f,
+      Clickable::pShape_->getLocalBounds().height / 2.0f);
+    Clickable::pShape_->setPosition(pos);
   }
 };
 
@@ -123,9 +102,8 @@ struct Wall : public Clickable<TLambda> {
 // GenerateDrawable
 //--------------------------------------------------------------------------------------------------------------
 
-template <typename TLambda>
-struct GenerateDrawable : public Clickable<TLambda> {
-  GenerateDrawable(Vector2f pos, Vector2f size, TLambda lambda, const std::string& text);
+struct GenerateDrawable : public Clickable {
+  GenerateDrawable(Vector2f pos, Vector2f size, std::function<Object*(Object*)> lambda, const std::string& text);
 
   void draw(RenderTarget& target, RenderStates states) const override {
     Object::draw(target, states);
@@ -138,39 +116,6 @@ private:
   Text text_;
 };
 
-//------ GenerateDrawable ------
-template <typename TLambda>
-GenerateDrawable<TLambda>::GenerateDrawable(Vector2f pos, Vector2f size, TLambda lambda, const std::string& text)
-  : Clickable<TLambda>(lambda) {
-  Clickable<TLambda>::pShape_ = std::make_shared<CircleShape>(50);
-  Clickable<TLambda>::pShape_->setPosition(pos);
-  Clickable<TLambda>::pShape_->setFillColor(Color::Red);
-
-  text_.setFont(font_);
-  text_.setCharacterSize(15); // Adjust the size if necessary
-  text_.setFillColor(Color::Black);
-  text_.setString(text);
-
-  // Center the text in the circle
-  FloatRect textRect = text_.getLocalBounds();
-  text_.setOrigin(textRect.left + textRect.width / 2.0f, textRect.top + textRect.height / 2.0f);
-  text_.setPosition(pos + Vector2f(10, 40));
-}
-
-//------ delay ------
-template <typename TLambda>
-bool GenerateDrawable<TLambda>::delay() const {
-  static auto start = std::chrono::high_resolution_clock::now();
-  static auto end = std::chrono::high_resolution_clock::now() + 500ms;
-
-  end = std::chrono::high_resolution_clock::now();
-  if (std::chrono::duration_cast<std::chrono::milliseconds>(end - start) > 500ms) {
-    start = std::chrono::high_resolution_clock::now();
-    return false;
-  }
-  return true;
-}
-
 //--------------------------------------------------------------------------------------------------------------
 // DistanceSensor
 //--------------------------------------------------------------------------------------------------------------
@@ -178,147 +123,55 @@ bool GenerateDrawable<TLambda>::delay() const {
 struct DistanceSensor : public Object {
   DistanceSensor() = default;
 
-  DistanceSensor(Vector2f direct, Vector2f pos, int radius);
+  DistanceSensor(Vector2f direct, Vector2f pos, int radius, std::vector<std::shared_ptr<Drawable>>* pWalls);
 
   DistanceSensor& operator = (const DistanceSensor& rhs);
 
-  std::optional<float> measureDistance(std::vector<std::shared_ptr<Drawable>>* pWalls);
+  float measureDistance() const;
 
   void draw(RenderTarget& target, RenderStates states) const override { Object::draw(target, states); }
 
-  std::optional<float> rectangleDistance(const RectangleShape& rectangle);
+  std::optional<float> rectangleDistance(const RectangleShape& rectangle) const;
+private:
+
+  std::vector<std::shared_ptr<Drawable>>* pWalls_;
 };
 
 //--------------------------------------------------------------------------------------------------------------
 // Car
 //--------------------------------------------------------------------------------------------------------------
 
-template <typename TLambda>
-struct Car : public Clickable<TLambda> {
-  Car(Vector2f pos, TLambda lambda);
+struct Car : public Clickable {
+  Car(Vector2f pos, std::function<Object*(Object*)> lambda, std::vector<std::shared_ptr<Drawable>>* pWalls);
 
   void update(std::vector<std::shared_ptr<Drawable>>* pWalls);
 
   void draw(RenderTarget& target, RenderStates states) const override;
 
   void move2(Vector2f pos) override { Object::move2(pos); updateSensorPositions(); }
-  void move(Vector2f pos) override { Object::move(pos); for (auto& sensor : sensors_) sensor.move(pos);}
+  void move(Vector2f direct) override {
+    Object::move(direct);
+    travelledDistance += std::sqrt(direct.x * direct.x + direct.y * direct.y);
+    for (auto& sensor : sensors_) sensor.move(direct);
+  }
 
   bool collides(FloatRect rect) override;
 
   std::vector<Vector2f> edges();
 
-  void updateSensorPositions();
-
   void turn(bool right) override;
 
+  const DistanceSensor& distanceSensor(int pos)         { return sensors_[pos]; }
+  int getTravelledDistance() { int ret = travelledDistance; travelledDistance = 0; return ret; }
+
 private:
-  std::vector<DistanceSensor> sensors_; // topLeft, topRight, bottomLeft, bottomRight, topMiddle
+  void updateSensorPositions();
   std::vector<Text> sensorsText_;
+  std::vector<DistanceSensor> sensors_; // topRight, topLeft, bottomRight, bottomLeft, topMiddle
 
   int radiusSensors = 10;
+  float travelledDistance{0};
 };
-
-//------ Car ------
-template <typename TLambda>
-Car<TLambda>::Car(Vector2f pos, TLambda lambda) : Clickable<TLambda>(lambda), sensorsText_(5) {
-  Clickable<TLambda>::pShape_ = std::make_shared<RectangleShape>(Vector2f{ 75, 100 });
-
-  Clickable<TLambda>::pShape_->setFillColor(Color::Blue);
-  Clickable<TLambda>::pShape_->setOrigin(Clickable<TLambda>::pShape_->getLocalBounds().width / 2.0f,
-    Clickable<TLambda>::pShape_->getLocalBounds().height / 2.0f);
-  Clickable<TLambda>::pShape_->setPosition(pos);
-
-  auto bounds = Clickable<TLambda>::pShape_->getGlobalBounds();
-  int radius = 10;
-  sensors_.push_back(DistanceSensor(Vector2f(1, 0), Vector2f(0, 0), radiusSensors));
-  sensors_.push_back(DistanceSensor(Vector2f(-1, 0), Vector2f(0, 0), radiusSensors));
-  sensors_.push_back(DistanceSensor(Vector2f(1, 0), Vector2f(0, 0), radiusSensors));
-  sensors_.push_back(DistanceSensor(Vector2f(-1, 0), Vector2f(0, 0), radiusSensors));
-  sensors_.push_back(DistanceSensor(Vector2f(0, -1), Vector2f(0, 0), radiusSensors));
-
-  int yPos = 300;
-  for (auto& text : sensorsText_) {
-    text.setFont(font_);
-    text.setCharacterSize(24);
-    text.setFillColor(Color::Black);
-    text.setPosition(Vector2f(1200, yPos));
-    yPos -= 50;
-  }
-
-  updateSensorPositions();
-}
-
-//------ update ------
-template <typename TLambda>
-void Car<TLambda>::update(std::vector<std::shared_ptr<Drawable>>* pWalls) {
-  int i = 0;
-  std::vector<std::string> names{"Top: ", "Left Bottom: ", "Right Bottom: ", "Left Top: ", "Right Top: " };
-  for (auto&& [sensor, name] : std::views::zip(sensors_, std::views::reverse(names))) {
-    float distance = sensor.measureDistance(pWalls).value_or(10000.0f);
-    sensorsText_[i].setString(name + std::to_string(distance));
-    ++i;
-  }
-}
-
-//------ draw ------
-template <typename TLambda>
-void Car<TLambda>::draw(RenderTarget& target, RenderStates states) const {
-  Object::draw(target, states);
-  for (auto& sensor : sensors_)
-    sensor.draw(target, states);
-
-  for (auto& text : sensorsText_)
-    target.draw(text);
-}
-
-//------ collides ------
-template <typename TLambda>
-bool Car<TLambda>::collides(FloatRect rect) {
-  for (auto& edge : edges()) {
-    if (rect.contains(edge))
-      return true;
-  }
-  return false;
-}
-
-//------ edges ------
-template <typename TLambda>
-std::vector<Vector2f> Car<TLambda>::edges() {
-  RectangleShape car = *((RectangleShape*)Object::pShape_.get());
-  return {
-    car.getTransform().transformPoint(Vector2f(0, 0)), // top left
-    car.getTransform().transformPoint(Vector2f(car.getSize().x, 0)), // top right
-    car.getTransform().transformPoint(Vector2f(0, car.getSize().y)), // bottom left
-    car.getTransform().transformPoint(Vector2f(car.getSize().x, car.getSize().y)), // bottom right
-    car.getTransform().transformPoint(sf::Vector2f(car.getSize().x / 2, 0)) // top middle
-  };
-}
-
-//------ updateSensorPositions ------
-template <typename TLambda>
-void Car<TLambda>::updateSensorPositions() {
-  auto bounds = Clickable<TLambda>::pShape_->getGlobalBounds();
-  sensors_[0].move2(Vector2f(edges()[0].x - radiusSensors, edges()[0].y - radiusSensors));
-  sensors_[1].move2(Vector2f(edges()[1].x - radiusSensors, edges()[1].y - radiusSensors));
-  sensors_[2].move2(Vector2f(edges()[2].x - radiusSensors, edges()[2].y - radiusSensors));
-  sensors_[3].move2(Vector2f(edges()[3].x - radiusSensors, edges()[3].y - radiusSensors));
-  sensors_[4].move2(Vector2f(edges()[4].x - radiusSensors, edges()[4].y - radiusSensors));
-}
-
-//------ turn ------
-template <typename TLambda>
-void Car<TLambda>::turn(bool right) {
-  Object::turn(right);
-  if (right)
-    ((RectangleShape*)(Object::pShape_.get()))->rotate(Object::turnSpeed_);
-  else
-    ((RectangleShape*)(Object::pShape_.get()))->rotate(-Object::turnSpeed_);
-
-  updateSensorPositions();
-  for (auto& sensor : sensors_)
-    sensor.turn(right);
-}
 
 //--------------------------------------------------------------------------------------------------------------
 // Simulator
@@ -335,24 +188,6 @@ struct Simulator {
   std::vector<std::shared_ptr<Drawable>> walls;
 };
 
-//------ Simulator ------
-Simulator::Simulator() : window(sf::VideoMode(1600, 1200), "Labyrinth") {
-  auto moveObj = [&](Object* pClickable) -> Object* {
-    pClickable->move2(window.mapPixelToCoords(Mouse::getPosition(window)));
-    return nullptr; };
-
-  auto createHWall = [moveObj](Object*) { return new Wall(Vector2f{ 400, 300 }, false, moveObj); };
-  auto createVWall = [moveObj](Object*) { return new Wall(Vector2f{ 400, 300 }, true, moveObj); };
-
-  std::shared_ptr<Drawable> horizontalGenerator(new GenerateDrawable(Vector2f{ 50, 50 }, Vector2f{ 50, 50 }, createHWall,
-    "   generate \nhorizontal wall"));
-  std::shared_ptr<Drawable> verticalGenerator(new GenerateDrawable(Vector2f{ 50, 150 }, Vector2f{ 50, 50 }, createVWall,
-    "   generate \nvertical wall"));
-  car = std::shared_ptr<Drawable>(new Car(Vector2f{500, 500}, moveObj));
-
-  clickables.push_back(std::move(horizontalGenerator));
-  clickables.push_back(std::move(verticalGenerator));
-  clickables.push_back(car);
-}
+} // end of namespace sf
 
 
